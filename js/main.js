324929e4217759239f117f0776607fa7e16a1273
thfql4136 <thfql4136@naver.com>
2018-11-28 11:34:42 +0900

    181128

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+/*********** 미이지 로더 ***************/
+$('body').imagesLoaded()
+.done( function( instance ) {
+	$(".loader").hide(0);
+  console.log('all images successfully loaded');
+})
+.progress( function( instance, image ) {
+  var result = image.isLoaded ? 'loaded' : 'broken';
+  console.log( 'image is ' + result + ' for ' + image.img.src );
+});
+
+
+/*********** firebase 초기 변수 ***************/
 var config = {
 	apiKey: "AIzaSyB7r-0ykSHtt1NRvyC_v9JbcBS5cHsZykM",
 	authDomain: "thfql4136-mall.firebaseapp.com",
@@ -502,6 +515,45 @@ $.ajax({
 	}
 });
 
+/*********** 왼쪽 카테고리 패널0번(furniture) ***************/
+var furniture = [];
+furniture[0] = [];
+furniture[1] = [];
+furniture[2] = [];
+furniture[3] = [];
+furniture[0][0] = "../img/main/menu-product-1-118x118.jpg";
+furniture[1][0] = "../img/main/menu-product-3-118x118.jpg";
+furniture[2][0] = "../img/main/menu-product-3-2-118x118.jpg";
+furniture[3][0] = "../img/main/menu-product-5-2-118x118.jpg";
+furniture[0][1] ="CLOCKS";
+furniture[1][1] ="TABLETOP";
+furniture[2][1] ="KITCHEN";
+furniture[3][1] ="LIGHTING";
+furniture[0][2] ="Mantel Clocks";
+furniture[1][2] ="Pepper Shakers";
+furniture[2][2] ="Oil Vineager Sets";
+furniture[3][2] ="Interior Lighting";
+furniture[0][3] ="Anniversary Clocks";
+furniture[1][3] ="Spice Jars";
+furniture[2][3] ="Bottle Racks";
+furniture[3][3] ="Celling Lamps";
+furniture[0][4] ="Wall Clocks";
+furniture[1][4] ="Dish Drainers";
+furniture[2][4] ="Chopping Boards";
+furniture[3][4] ="Wall Lamps";
+furniture[0][5] ="Digital Clocks";
+furniture[1][5] ="Cocktail Shakers";
+furniture[2][5] ="Vacuum Flasks";
+furniture[3][5] ="Floor Lamps";
+furniture[0][6] ="Travel and Alarm";
+furniture[1][6] ="Utensil Holders";
+furniture[2][6] ="Utensil Holders";
+furniture[3][6] ="Celling Lamps";
+var furnitureBrand = [];
+furnitureBrand[0] ="../img/main/brand-alessi.png";
+furnitureBrand[1] ="../img/main/brand-Eva-Solo.png";
+furnitureBrand[2] ="../img/main/brand-PackIt.png";
+furnitureBrand[3] ="../img/main/brand-witra.png";
 
 /********** 왼쪽 카테고리 생성 **************/
 var sFn = function(data){
@@ -512,10 +564,45 @@ var sFn = function(data){
 			html = '<li>';
 			html += '<span class="'+rs.icon+'"></span>';
 			html += '<a href="'+rs.link+'"><span>'+rs.title+'</span></a>';
-			if(rs.ajax !== '') html += '<span class="fas fa-angle-right"></span>';
-			html +='</li>'
+			if(rs.ajax !== '') {
+			html += '<span class="fas fa-angle-right"></span>';
+			html+= '<div class="cate_panel clear">';
+			/********** 패널 생성.시작 **************/
+			if(i==0) {
+				for(var j=0; j<furniture.length; j++){
+					html+='<ul id="fur_panel'+i+'" class="fur_panel">';
+					html+='<li><img src="'+furniture[j][0]+'" class="img"></img></li>';
+					html+='<li>'+furniture[j][1]+'</li>';
+					html+='<li>'+furniture[j][2]+'</li>';
+					html+='<li>'+furniture[j][3]+'</li>';
+					html+='<li>'+furniture[j][4]+'</li>';
+					html+='<li>'+furniture[j][5]+'</li>';
+					html+='<li>'+furniture[j][6]+'</li>';
+					html+='</ul>';
+				}	
+				html+= '<ul class="fur_brand_panel clear">';
+				for(var j=0; j<furnitureBrand.length; j++){
+					html +='<li><img src="'+furnitureBrand[j]+'" class="img w3-grayscale-max w3-opacity"></li>';
+					
+				}
+				html +='</ul>';
+			}
+			/********** 패널 생성.종료 **************/
+			html +='</div>';
+			}
+			html +='</li>';
 			$(".banners .cate").append(html);
 		}
+		$(".cate > li").hover(function(){
+			$(this).find(".cate_panel").show();
+		}, function(){
+			$(this).find(".cate_panel").hide();
+		});
+		$(".fur_brand_panel img").hover(function(){
+			$(this).removeClass("w3-grayscale-max w3-opacity");
+		}, function(){
+			$(this).addClass("w3-grayscale-max w3-opacity");
+		});
 	}
 }
 
@@ -554,4 +641,19 @@ $(".banner").mousemove(function (evt) {  //클래스배너에서 마우스가 �
     $(this).find(".ban_img").css("transform", "translate("+mX+"px, "+mY+"px)");
     
 });
+
+/********** Featured ***********/
+/* $(".featured_item").hover(function(){
+	$(this).find("h5").stop().slideDown(200);
+}, function(){
+	$(this).find("h5").stop().slideUp(200);
+}); */
+$(".featured_item").hover(function(){
+	$(this).find("div").stop().animate({"bottom":0}, 200);
+	$(this).find("img").css({"animation-name":"featuredAni"});
+}, function(){
+	$(this).find("div").stop().animate({"bottom":"-3rem"},200);
+	$(this).find("img").css({"animation-name":"featuredAniBack"});
+}
+);
 
